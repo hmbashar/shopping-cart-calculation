62b6d483e31e3437138d6ad8cf571efe7e8b66b7
hmbashar <hmbashar@gmail.com>
2023-02-23 19:55:16 +0600

    phone quantity and price update

---
+++ js/phone.js
@@ -0,0 +1,45 @@
+function updateCaseNumber (isIncrease) {
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+    
+    let newPhoneNumber;
+
+    if(isIncrease === true) {
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else {
+        newPhoneNumber = previousPhoneNumber - 1;
+    }   
+
+    phoneNumberField.value = newPhoneNumber;
+
+    return newPhoneNumber;
+}
+
+//Price Update
+function updateCaseTotalPrice(newPhoneNumber) {
+    
+    const phoneTotalPrice = newPhoneNumber * 1219 ;
+
+    const phoneTotalAmount = document.getElementById('phone-price-amount');
+
+    phoneTotalAmount.innerText = phoneTotalPrice;
+}
+
+
+
+document.getElementById('btn-phone-plus').addEventListener('click', function() {
+    const totalPhonePrice = updateCaseNumber(true);
+
+    updateCaseTotalPrice(totalPhonePrice);
+
+});
+
+
+document.getElementById('btn-phone-minus').addEventListener('click', function() {
+    const totalPhonePrice = updateCaseNumber(false);
+    
+    updateCaseTotalPrice(totalPhonePrice);
+
+});
